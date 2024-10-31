6e4303ed4f4bb80c50b8d41177bb0adcc70065ee
Apple <Apple@MacBook-Pro.local>
2024-10-31 19:11:44 +0200

    update review model and add config

--- app/models/request_models.py
+++ app/models/request_models.py
@@ -1,7 +1,8 @@
 from pydantic import BaseModel, HttpUrl
+from typing import Literal
 
 
 class ReviewRequest(BaseModel):
     assignment_description: str
     github_repo_url: HttpUrl
-    candidate_level: "Junior" | "Middle" | "Senior"
+    candidate_level: Literal["Junior", "Middle", "Senior"]
